d2a85e601c2a046e3f83e7f3f2be6758f490256f
Mike <48353088@qq.com>
2015-04-29 12:33:18 +0800

    删除缩略图功能，统一使用more标签输出

--- archive.php
+++ archive.php
@@ -1,3 +1,4 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
   <div class="main">
     <div class="left">
@@ -6,22 +7,13 @@
 </div>
 	<?php if ($this->have()): ?>
 	<?php while($this->next()): ?>
-      <div class="cont" <?php if ($this->options->thumbDisplay == 'display') { ?>style="height:210px"<?php } ?>>
+      <div class="cont">
         <h2><a href="<?php $this->permalink() ?>"><?php $this->sticky(); $this->title() ?></a></h2>
         <p class="entry_data">发布时间：<?php $this->date('Y年n月j日'); ?> / 分类：<?php $this->category(','); ?> / <?php Views_Plugin::theViews('', ' 次围观'); ?> / <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('抢沙发！', '一次吐槽', '%d 次吐槽'); ?></a></p>
         <div class="list-content">
-		<?php if ($this->options->thumbDisplay == 'display') { ?>
-<div class="thumbnail_box">
-<div class="thumbnail">
-<a href="<?php $this->permalink() ?>" title="<?php $this->title() ?>">
-<img src="<?php $this->options->themeUrl('timthumb.php'); ?>?src=<?php echo img_postthumb($this->cid); ?>&h=100&w=140&zc=1"/>
-</a></div></div>
-<div class="entry-content"><?php $this->excerpt(140, '...'); ?> </div>
-<?php } ?>
-		<?php if ($this->options->thumbDisplay == 'close') { ?>
-<?php $this->excerpt(140, '...'); ?> 
-<?php } ?>
-</div></div>
+          <?php $this->content('- 阅读剩余部分 -'); ?>
+        </div>
+      </div>
 	<?php endwhile; ?>
     <?php else: ?>
         <div class="cont">
@@ -31,5 +23,4 @@
       <?php $this->pageNav(); ?>
     </div>
 <?php $this->need('sidebar.php'); ?>
-  </div>
 <?php $this->need('footer.php'); ?>
